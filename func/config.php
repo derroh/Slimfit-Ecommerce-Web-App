7fc35e50041b9ab2ec404b2e5cb953cd5859668b
derroh <abucheriwitness@gmail.com>
2017-10-04 18:21:19 +0300

    Add files via upload

---
+++ func/config.php
@@ -0,0 +1,46 @@
+<?php
+ob_start();
+session_start();
+
+//database credentials
+define('DBHOST','localhost');
+define('DBUSER','root');
+define('DBPASS','');
+define('DBNAME','slimfitshop');
+
+$db = new PDO("mysql:host=".DBHOST.";port=3306;dbname=".DBNAME, DBUSER, DBPASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//set timezone
+date_default_timezone_set('Africa/Nairobi');
+
+//load classes as needed
+function __autoload($class) {
+
+   $class = strtolower($class);
+
+	//if call from within assets adjust the path
+   $classpath = 'classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	}
+
+	//if call from within admin adjust the path
+   $classpath = '../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	}
+
+	//if call from within admin adjust the path
+   $classpath = '../../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	}
+
+}
+
+$user = new User($db);
+
+
+?>
